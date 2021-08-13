3e9645fe65d07556bff6ab67a6c8c142aeb4c32c
sahaym2 <sahaym2@medtronic.com>
2021-08-13 12:18:34 +0300

    Initial commit

---
+++ Sources/App/Controllers/DefaultBotController.swift
@@ -0,0 +1,69 @@
+//
+//  File.swift
+//  
+//
+//  Created by Maksym Sahaydak on 12.08.2021.
+//
+
+import Vapor
+import telegram_vapor_bot
+
+struct T: Decodable {
+    let text: String
+}
+
+
+final class DefaultBotHandlers {
+    
+    static func addHandlers(app: Vapor.Application, bot: TGBotPrtcl) {
+        defaultHandler(app: app, bot: bot)
+        commandPingHandler(app: app, bot: bot)
+        commandCatHandler(app: app, bot: bot)
+    }
+    
+    
+    private static func commandCatHandler(app: Vapor.Application, bot: TGBotPrtcl) {
+        let handler = TGCommandHandler(commands: ["/cat"]) { update, bot in
+            app.http.client.shared.get(url: "https://cat-fact.herokuapp.com/facts").whenSuccess { result in
+                guard let body = result.body, let data = body.getData(at: 0, length: body.readableBytes) else {
+                    return
+                }
+                let jsonResponse = try? JSONSerialization.jsonObject(with: data, options: [])
+                guard let jsonArray = jsonResponse as? [[String: Any]],
+                      let title = jsonArray.randomElement()?["text"] as? String else {
+                      return
+                }
+                try? update.message?.reply(text: title, bot: bot)
+            }
+        }
+        bot.connection.dispatcher.add(handler)
+    }
+    
+    
+    
+    private static func defaultHandler(app: Vapor.Application, bot: TGBotPrtcl) {
+        let handler = TGMessageHandler(filters: (.all && !.command.names(["/ping"]))) { update, bot in
+             let params: TGSendMessageParams = .init(chatId: .chat(update.message!.chat.id), text: "Success")
+             try bot.sendMessage(params: params)
+            if let mediaID =  update.message?.photo?.last?.fileId {
+                try? bot.getFile(params: TGGetFileParams(fileId: mediaID)).whenSuccess({ file in
+                    guard let filePath = file.filePath else {
+                        return
+                    }
+                    let tgApi: String = "1900421527:AAFBdi3x4M1y1WGFfqSuBbf_k5iNsqMPmhI"
+                    let url = "https://api.telegram.org/file/bot\(tgApi)/\(filePath)"
+                    try? update.message?.reply(text: url, bot: bot)
+                })
+            }
+            
+        }
+        bot.connection.dispatcher.add(handler)
+    }
+    
+    private static func commandPingHandler(app: Vapor.Application, bot: TGBotPrtcl) {
+        let handler = TGCommandHandler(commands: ["/ping"]) { update, bot in
+            try update.message?.reply(text: "pong", bot: bot)
+        }
+        bot.connection.dispatcher.add(handler)
+    }
+}
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,12 +1,30 @@
 import Fluent
 import FluentPostgresDriver
 import Vapor
+import telegram_vapor_bot
 
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
+    
+    // MARK: - App
+    
+    app.http.server.configuration.hostname = "0.0.0.0"
+    app.http.server.configuration.port = 80
+    
+    // MARK: - Telegram
+    
+    let tgApi: String = "1900421527:AAFBdi3x4M1y1WGFfqSuBbf_k5iNsqMPmhI"
+    let connection: TGConnectionPrtcl = TGLongPollingConnection()
+    TGBot.configure(connection: connection, botId: tgApi, vaporClient: app.client)
+    try TGBot.shared.start()
+    
+    TGBot.log.logLevel = .error
+    
+    DefaultBotHandlers.addHandlers(app: app, bot: TGBot.shared)
+    // MARK: - Database
+    
     app.databases.use(.postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
